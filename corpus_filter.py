17a37936a547188c3c5cacc04ed09814ace88b36
asmaier <asmaier@web.de>
2020-05-24 23:56:27 +0200

    Added commandline options with argparse.

--- corpus_filter.py
+++ corpus_filter.py
@@ -1,13 +1,9 @@
 import csv
 import re
+import argparse
 
 import stanza
 from spacy_stanza import StanzaLanguage
-
-LEMMAS = "lemmas_russian_a1.csv"
-SENTENCES = "sentences_russian_a1.csv" 
-FILTERED = "filtered_russian_a1.csv"
-
 
 def has_only_lemmas(text, lemmas):
 	clean_text = "".join(c for c in text if c.isalpha() or c == " ")
@@ -23,29 +19,40 @@ def has_only_lemmas(text, lemmas):
 #	print(" ".join([token.lemma_ for token in doc]))
 	return True
 
+if __name__ == "__main__":
 
-stanza.download('ru')  # will take a while
-
-snlp = stanza.Pipeline(lang="ru")
-nlp = StanzaLanguage(snlp)
-
-lemmas = set()
-with open(LEMMAS, 'r') as infile:
-	reader = csv.reader(infile, delimiter=',', quotechar='"')
-	for row in reader:
-		lemmas.add(row[0])
-
-count = 0
-with open(SENTENCES, 'r') as infile:
-	reader = csv.reader(infile, delimiter=',', quotechar='"')
-	with open(FILTERED, "w") as outfile:
-		writer = csv.writer(outfile, delimiter=',', quotechar='"')
-		for row in reader:
-			sentence = row[0]
-			count+=1
-			if has_only_lemmas(row[0],lemmas):
-				writer.writerow([sentence])
-				outfile.flush()				
-				print(count, sentence) 
+	parser = argparse.ArgumentParser(description="Corpus filter - filters a corpus of sentences according to a white list of lemmas.")
+	parser.add_argument("lemmas", type=str, help="Lemmas file - white list of lemmas as csv file")
+	parser.add_argument("input", type=str, help="Input file - list of sentences as csv file")
+	parser.add_argument("output", type=str, help="Output file - filtered list of sentences as csv file")
+	args = parser.parse_args()
 	
+	LEMMAS = args.lemmas
+	SENTENCES = args.input
+	FILTERED = args.output
+
+	stanza.download('ru')  # will take a while
+
+	snlp = stanza.Pipeline(lang="ru")
+	nlp = StanzaLanguage(snlp)
+
+	lemmas = set()
+	with open(LEMMAS, 'r') as infile:
+		reader = csv.reader(infile, delimiter=',', quotechar='"')
+		for row in reader:
+			lemmas.add(row[0])
+
+	count = 0
+	with open(SENTENCES, 'r') as infile:
+		reader = csv.reader(infile, delimiter=',', quotechar='"')
+		with open(FILTERED, "w") as outfile:
+			writer = csv.writer(outfile, delimiter=',', quotechar='"')
+			for row in reader:
+				sentence = row[0]
+				count+=1
+				if has_only_lemmas(row[0],lemmas):
+					writer.writerow([sentence])
+					outfile.flush()				
+					print(count, sentence) 
+		
 
